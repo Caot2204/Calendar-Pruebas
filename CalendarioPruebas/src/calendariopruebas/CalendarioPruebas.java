bb26318bcd27672d4dac9aa7cbf39d3b74989175
Caot22 <caot2204@gmail.com>
2018-11-25 13:42:09 -0600

    Calendario sin funcionalidad

---
+++ CalendarioPruebas/src/calendariopruebas/CalendarioPruebas.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calendariopruebas;
+
+/**
+ *
+ * @author Carlos Onorio
+ */
+public class CalendarioPruebas {
+    
+    String fecha;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void leerFecha() {
+               
+    }
+    
+    public void determinarSiguienteDia() {
+        
+    }
+    
+    public void determinarDiaSemana() {
+        
+    }
+    
+    public void determinarZodiaco() {
+        
+    }
+    
+    public void determinarMemorialDay() {
+        
+    }
+    
+    public void determinarViernes13() {
+        
+    }
+    
+}
